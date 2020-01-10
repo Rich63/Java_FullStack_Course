e93a86022c7dc2c568f86dd87b900ef0885c73d6
Rich63 <rich63-develop@outlook.com>
2020-01-10 15:29:01 +0100

    Completed Section 3

---
+++ Section-3/Excersize/Exercise.java
@@ -0,0 +1,24 @@
+/**
+ * Exercise
+ */
+public class Exercise {
+
+    public static void main(String[] args) {
+      //(a+b)square = ((a*a) + 2ab + (b*b))
+      // int a = 25;
+      // float b = 42.159f;
+      // int value;
+
+      // value = (int)(a*a + 2*(a*b) + b*b);
+      // System.out.println("Value off the equation is " + value);
+
+      //int number1= 10.128952248;
+      float number2 = 10.128952248f;
+      double number3 = 10.128952248d;
+  
+      //System.out.println(number1);
+      System.out.println(number2);
+      System.out.println(number3);
+
+    }
+}
