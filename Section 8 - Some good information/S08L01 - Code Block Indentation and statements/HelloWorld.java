63a88993833c6d3b1336ef5ec770c6778bce5835
Rich63 <rich63-develop@outlook.com>
2020-01-16 16:49:33 +0100

    Finished Section 8

---
+++ Section 8 - Some good information/S08L01 - Code Block Indentation and statements/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * HelloWorld
+ */
+public class HelloWorld {  // This is begin of Code Block
+
+  public static void main(String[] args) {  // This is begin of Code Block
+    int year = 2020;  // This is a statement
+
+    System.out.println("Hello, World "+year);  // This is a statement 
+
+    for(int i=1;i<=3;i++) {  // This is begin of Code Block
+      System.out.println("Checking value of i in for loop of every iteration");  // This is a statement
+      System.out.println(i);  // This is a statement and variable i is only used in this code block
+    }  // This is end of Code Block
+
+    int i=1;  // This is a statement and variable i is only used in this code block
+    while(i<=3) {  // This is begin of Code Block
+      System.out.println("Checking value of i in while loop of every iteration");  // This is a statement
+      System.out.println(i);  // This is a statement
+      i++;  // This is a statement
+    }  // This is end of Code Block
+  }  // This is end of Code Block
+}  // This is end of Code Block
