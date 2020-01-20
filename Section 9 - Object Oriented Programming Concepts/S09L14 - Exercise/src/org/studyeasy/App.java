a07c906fd50fce5088254f5855031c8bd8b09131
Rich63 <rich63-develop@outlook.com>
2020-01-20 16:49:07 +0100

    After lesson 75 of Section 9

--- Section 9 - Object Oriented Programming Concepts/S09L14 - Exercise/src/org/studyeasy/App.java
+++ Section 9 - Object Oriented Programming Concepts/S09L14 - Exercise/src/org/studyeasy/App.java
@@ -13,20 +13,20 @@ import org.studyeasy.species.animals.Eel;
 public class App {
 
   public static void main(String[] args) {
-    // Animal animal = new Animal();
-    // System.out.println(animal.showInfo());
+    Animal animal = new Animal();
+    System.out.println(animal.showInfo());
 
-    // Reptile reptile = new Reptile();
-    // System.out.println(reptile.showInfo());
+    Reptile reptile = new Reptile();
+    System.out.println(reptile.showInfo());
     
-    // Crocodile crocodile = new Crocodile();
-    // System.out.println(crocodile.showInfo());
+    Crocodile crocodile = new Crocodile();
+    System.out.println(crocodile.showInfo());
     
-    // Fish fish = new Fish();
-    // System.out.println(fish.showInfo());
+    Fish fish = new Fish();
+    System.out.println(fish.showInfo());
     
-    // Eel eel = new Eel();
-    // System.out.println(eel.showInfo());
+    Eel eel = new Eel();
+    System.out.println(eel.showInfo());
     
     Eagle eagle = new Eagle();
     System.out.println(eagle.showInfo());
